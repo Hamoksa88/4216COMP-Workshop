553d84706eb04138a9f67431e9be6b3c727793b9
hubbacrack <jeremy.huxleyr@gmail.com>
2022-03-15 13:20:06 +0000

    stuff

--- Program.py
+++ Program.py
@@ -2,11 +2,19 @@
 
 #Get data from files:
 
+from asyncio import protocols
+
+
 passengerKilometres = open("PassengerKilometres.csv", "r").readlines()
 pkYears = []
 pkDistance = []
 
 passengerRevenue = open("PassengerRevenueByTicketType.csv", "r").readlines()
+prYears = []
+prAdvanced = []
+prPeak = []
+prOffPeak = []
+prOther = []
 
 for line in passengerKilometres: pkYears.append(line[:4])
 for i in range(0,3): pkYears.pop(0)
@@ -19,3 +27,20 @@ for line in passengerKilometres:
     except: pkDistance.append("N/A")
 for i in range(0,3): pkDistance.pop(0)
 pkDistance.pop(-1)
+
+
+for line in passengerRevenue[3:38]:
+    temp = []
+    #if line in [passengerRevenue[0], passengerRevenue[1], passengerRevenue[2]]: continue
+    temp = line.split(",")
+    
+    
+    print(temp)
+
+    prYears.append(temp[0])
+    prAdvanced.append(temp[1])
+    prPeak.append(temp[2])
+    prOffPeak.append(temp[3])
+    prOther.append(temp[4])
+
+    
