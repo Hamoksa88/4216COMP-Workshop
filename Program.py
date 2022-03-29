1315dda0d8f6a219855f9a52b82b32581f57aa9a
hubbacrack <jeremy.huxleyr@gmail.com>
2022-03-29 13:55:12 +0100

    Removed glitch and fixed bugs version 1.9

--- Program.py
+++ Program.py
@@ -1,37 +1,126 @@
-
-
-#Get data from files:
+##Import modules:
 
 from asyncio import protocols
 import csv
 
+
+##Get data from files:
+
 passengerKilometres = open("PassengerKilometres.csv", "r").readlines()
-pkYears = []
-pkDistance = []
+pkData = []
 
 passengerRevenue = open("PassengerRevenueByTicketType.csv")
-prYears = []
-prAdvanced = []
-prPeak = []
-prOffPeak = []
-prOther = []
+prData = []
+prYearly = []
+prQuarterly = []
 
-for line in passengerKilometres: pkYears.append(line[:4])
-for i in range(0,3): pkYears.pop(0)
-pkYears.pop(-1)
+dis_reader = csv.reader(passengerKilometres, delimiter=',', quotechar='"')
+for row in dis_reader: pkData.append(row)
+for i in range(0,3): pkData.pop(0)
+pkData.pop(-1)
+#for r in pkData: print(r)
 
-for line in passengerKilometres:
-    try: 
-        try: pkDistance.append(float(line[5:9]))
-        except: pkDistance.append(float(line[8:12]))
-    except: pkDistance.append("N/A")
-for i in range(0,3): pkDistance.pop(0)
-pkDistance.pop(-1)
+rev_reader = csv.reader(passengerRevenue, delimiter=',', quotechar='"')
+for row in rev_reader: prData.append(row)
+for i in range(0,3): prData.pop(37)
+for i in range(0,2): prData.pop(0)
+#for r in prData: print(r)
+
+for i in range(0,35): prYearly.append(prData[i])
+for i in range(35,len(prData)): prQuarterly.append(prData[i])
+#for r in prYearly: print(r)  
+#for r in prQuarterly: print(r)
 
 
+##Functions:
 
-csv_reader = csv_reader(passengerRevenue, delimiter=',', quotechar='"')
-i = 0
-for row in csv_reader:
-    if i in [1,2,3]: i += 1 continue else: i += 1 print(row)
+def pkMenu():
+    print("Welcome!")
+    print("Data Visualisation on a Graph (1)")
+    print("Mean Values of Data (2)")
+    print("Highest and lowest values of data (3)")
+    print("View the data (4)")
+    user_input = input("What would you like to access?")
+
+    if(user_input == '1'):
+        #------Insert Graph Program Here-----
+        print("You Have Chosen Data Visualisation on a Graph")
+
+    elif(user_input == '2'):
+        #------Insert Mean Program Here------
+        print("You Have Chosen Mean Values of Data")
+        pk_Mean()
+
+    elif(user_input == '3'):
+        print("You Have Chosen highest and lowest values of data.")
+        pk_HighestLowest()
+        
+    elif(user_input == '4'):
+        #------Insert Data Viewing Program Here------
+        print("You Have Chosen to View the Data")
+    else:
+        print('You Have Entered an Incorrect Value')
+        pkMenu()
+
+def prMenu():
+    print('''Welome to the "Passenger Revenue from tickets" menu!
+    Graph visualisation of data (1)
+    Mean values of data (2)
+    Lowest and highest values of data (3)
+    ''')
+    user_input = input("What would you like to access?")
     
+    if user_input == '1':
+        print("You have selected graph visualisation of data.")
+    
+    elif user_input == '2':
+        print("You have selected the mean amounts of data.")
+    
+    elif user_input == '3':
+        print("You have selected the lowest and highest amounts of data.")
+    
+    else:
+        print("You have entered an incorrect value.")
+        prMenu()
+
+def pk_Mean():
+    temp = []
+    for val in pkData:
+        if val[1] != ":":
+            temp.append(float(val[1]))
+
+    mean = sum(temp) / len(temp)
+    print("The mean kilometres travelled from 1938-2021 is", mean)
+
+def pk_HighestLowest():
+    temp = []
+    year = []
+    for val in pkData:
+        if val[1] != ":":
+            temp.append(float(val[1]))
+            year.append(val[0])
+
+    highestYear = year[temp.index(max(temp))]
+    lowestYear = year[temp.index(min(temp))]
+    print("The highest and lowest distances travelled were", max(temp), "and", min(temp), "in", highestYear, "and", lowestYear, "respectively.")
+
+def pr_Mean():
+    prY_OTA = []
+    for val in prYearly:
+        if val[1] != "[x]": prY_OTA.append(val[1])
+    print("Average yearly revenue for ordinary ticket advanced:", sum(prY_OTA) / len(prY_OTA))
+
+    prY_OTAoP = []
+    for val in prYearly:
+        if val[1] != "[x]": prY_OTAoP.append(val[1])
+    print("Average yearly revenu for ordinary ticket anytime or peak:", sum(prY_OTAoP) / len(prY_OTAoP))
+
+    prY_OTOP = []
+    for val in prYearly:
+        if val[1] != "[x]": prY_OTOP.append(val[1])
+    print("Average yearly revenue for ordinary ticket off peak:", sum(prY_OTOP) / len(prY_OTOP))
+    
+
+##Main:
+
+pkMenu()
