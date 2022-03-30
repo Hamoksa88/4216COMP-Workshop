3edbd6e727db5d0ac63af2ca281e18c62df1b00e
Hamoksa88 <96214886+Hamoksa88@users.noreply.github.com>
2022-03-30 20:45:21 +0200

    Add files via upload

---
+++ graph_Visualisation_For_TicketType_against_Revenue_inMillion.py
@@ -0,0 +1,103 @@
+import matplotlib.pyplot as plt
+   
+Ticket_Type = ['ticket Advance','ticket Anytime or Peak','ticket Off Peak','ticket Other','ordinary ticketotal','Season ticket','passenger revenue','pass rev at November 2020 prices','Nonfranchised pass rev']
+
+Revenue_for_Apr_2010_to_Mar2011 = [804,1903,2126,132,4965,1654,6620,7983,150.9]
+plt.figure(figsize=(10, 3))
+plt.bar(Ticket_Type,Revenue_for_Apr_2010_to_Mar2011)
+plt.title('revenue according to year Apr 2010 to Mar 2011')
+plt.xlabel('Year')
+plt.ylabel('revenue by ticket type( in millions)')
+plt.show()
+
+Ticket_Type = ['ticket Advance','ticket Anytime or Peak','ticket Off Peak','ticket Other','ordinary ticketotal','Season ticket','passenger revenue','pass rev at November 2020 prices','Nonfranchised pass rev']
+
+Revenue_for_Apr_2011_to_Mar2012 = [965,2062,2286,134,5447,1782,7229,8321,157.1]
+plt.figure(figsize=(10, 3))
+plt.bar(Ticket_Type,Revenue_for_Apr_2011_to_Mar2012)
+plt.title('revenue according to year Apr 2011 to Mar 2012')
+plt.xlabel('Year')
+plt.ylabel('revenue by ticket type( in millions)')
+plt.show()
+
+Ticket_Type = ['ticket Advance','ticket Anytime or Peak','ticket Off Peak','ticket Other','ordinary ticketotal','Season ticket','passenger revenue','pass rev at November 2020 prices','Nonfranchised pass rev']
+
+Revenue_for_Apr_2012_to_Mar2013 = [1058,2144,2475,139,5816,1890,7707,8634,167.0]
+plt.figure(figsize=(10, 3))
+plt.bar(Ticket_Type,Revenue_for_Apr_2012_to_Mar2013)
+plt.title('revenue according to year Apr 2012 to Mar 2013')
+plt.xlabel('Year')
+plt.ylabel('revenue by ticket type( in millions)')
+plt.show()
+
+Revenue_for_Apr_2013_to_Mar2014 = [1154,2269,2591,148,6162,2041,8203,9005,179.4]
+plt.figure(figsize=(10, 3))
+plt.bar(Ticket_Type,Revenue_for_Apr_2013_to_Mar2014)
+plt.title('revenue according to year Apr 2013 to Mar 2014')
+plt.xlabel('Year')
+plt.ylabel('revenue by ticket type( in millions)')
+plt.show()
+
+Revenue_for_Apr_2014_to_Mar2015 = [1293,2436,2755,166,6649,2153,8803,9577,191.8]
+plt.figure(figsize=(10, 3))
+plt.bar(Ticket_Type,Revenue_for_Apr_2014_to_Mar2015)
+plt.title('revenue according to year Apr 2014 to Mar 2015')
+plt.xlabel('Year')
+plt.ylabel('revenue by ticket type( in millions)')
+plt.show()
+
+Revenue_for_Apr_2015_to_Mar2016 = [1314,2647,2903,143,7008,2205,9213,10003,187.6]
+plt.figure(figsize=(10, 3))
+plt.bar(Ticket_Type,Revenue_for_Apr_2015_to_Mar2016)
+plt.title('revenue according to year Apr 2015 to Mar 2016')
+plt.xlabel('Year')
+plt.ylabel('revenue by ticket type( in millions)')
+plt.show()
+
+Revenue_for_Apr_2016_to_Mar2017 = [1426,2738,2986,120,7269,2171,9441,10139,195.7]
+plt.figure(figsize=(10, 3))
+plt.bar(Ticket_Type,Revenue_for_Apr_2016_to_Mar2017)
+plt.title('revenue according to year Apr 2016 to Mar 2017')
+plt.xlabel('Year')
+plt.ylabel('revenue by ticket type( in millions)')
+plt.show()
+
+Revenue_for_Apr_2017_to_Mar2018 = [1554,2843,3124,62,7584,2072,9655,10052,198.4]
+plt.figure(figsize=(10, 3))
+plt.bar(Ticket_Type,Revenue_for_Apr_2017_to_Mar2018)
+plt.title('revenue according to year Apr 2017 to Mar 2018')
+plt.xlabel('Year')
+plt.ylabel('revenue by ticket type( in millions)')
+plt.show()
+
+Revenue_for_Apr_2018_to_Mar2019 = [1711,3057,3319,19,8106,2136,10241,10423,199.6]
+plt.figure(figsize=(10, 3))
+plt.bar(Ticket_Type,Revenue_for_Apr_2018_to_Mar2019)
+plt.title('revenue according to year Apr 2018 to Mar 2019')
+plt.xlabel('Year')
+plt.ylabel('revenue by ticket type( in millions)')
+plt.show()
+
+Revenue_for_Apr_2018_to_Mar2019 = [1711,3057,3319,19,8106,2136,10241,10423,199.5]
+plt.figure(figsize=(10, 3))
+plt.bar(Ticket_Type,Revenue_for_Apr_2018_to_Mar2019)
+plt.title('revenue according to year Apr 2018 to Mar 2019')
+plt.xlabel('Year')
+plt.ylabel('revenue by ticket type( in millions)')
+plt.show()
+
+Revenue_for_Apr_2019_to_Mar2020 = [1736,3074,3379,-56,8133,2076,10208,10246,189.5]
+plt.figure(figsize=(10, 3))
+plt.bar(Ticket_Type,Revenue_for_Apr_2019_to_Mar2020)
+plt.title('revenue according to year Apr 2019 to Mar 2020')
+plt.xlabel('Year')
+plt.ylabel('revenue by ticket type( in millions)')
+plt.show()
+
+Revenue_for_Apr_2020_to_Mar2021 = [303,504,708,45,1560,333,1894,1894,12.1]
+plt.figure(figsize=(10, 3))
+plt.bar(Ticket_Type,Revenue_for_Apr_2020_to_Mar2021)
+plt.title('revenue according to year Apr 2020 to Mar 2021')
+plt.xlabel('Year')
+plt.ylabel('revenue by ticket type( in millions)')
+plt.show()
