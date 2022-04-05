c7d572e57f1d68145ff532a919c609d48f59c6ae
DanLeech1 <D.Leech@2021.ljmu.ac.uk>
2022-04-05 15:56:57 +0100

    update

--- MainProgram.py
+++ MainProgram.py
@@ -147,7 +147,8 @@ def pk_DataVisualisation():
 
     ax.set_ylabel("Kilometres (Billions)", fontsize=12)
 
-    ax.plot(year, kilometres, 'ro-')
+    ax.plot(year, kilometres, 'ro-', label="Kilometres Travelled (Billions)")
+    ax.legend(loc="upper left")
 
     ax.xaxis.grid()
     ax.yaxis.grid()
@@ -200,11 +201,12 @@ def pr_DataVisualisation():
 
     ax1.set_ylabel("(£ million)", fontsize=12)
 
-    ax1.plot(year2, ticket)
-    ax1.plot(year2, s_ticket)
+    ax1.plot(year2, ticket, label="Regular Ticket Revenue")
+    ax1.plot(year2, s_ticket, label="Seasonal Ticket Revenue")
 
     ax1.xaxis.grid()
     ax1.yaxis.grid()
+    ax1.legend(loc="upper left")
 
     plt.show()
 
